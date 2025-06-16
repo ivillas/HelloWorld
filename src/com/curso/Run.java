e2f7c92faf18820f10384769abdf5a5c3c441b2d
Ivan Villa <ivifo@hotmail.com>
2025-06-16 15:21:10 +0200

    Solucionado error

--- src/com/curso/Run.java
+++ src/com/curso/Run.java
@@ -4,7 +4,7 @@ public class Run {
 
 	public static void main(String[] args) {
 		System.out.println("In english: Hello World!");
-		System.out.println("In english: By World!")
+		System.out.println("In english: By World!");
 		System.out.println("En español: Hola Mundo!");
 		System.out.println("En español: Adios Mundo!");
 	}
