5d042abb6307166c25539f1b97f8039ff094154f
Ivan Villa <ivifo@hotmail.com>
2025-06-16 22:08:32 +0200

    Añadida entrada teclado

--- src/com/curso/Run.java
+++ src/com/curso/Run.java
@@ -1,5 +1,7 @@
 package com.curso;
 
+import java.util.Scanner;
+
 public class Run {
 
 	public static void main(String[] args) {
@@ -7,6 +9,16 @@ public class Run {
 		System.out.println("In english: By World!");
 		System.out.println("En español: Hola Mundo!");
 		System.out.println("En español: Adios Mundo!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserta tu nombre: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Hola, " + name + "!");
+
+        scanner.close();
 	}
 
 }
+// This is a simple Java program that prints greetings in English and Spanish.
