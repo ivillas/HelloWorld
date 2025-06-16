5297dd77713cf29f68d9efba35b99e05c6796b58
Ivan Villa <ivifo@hotmail.com>
2025-06-16 15:04:30 +0200

    Actualización mensaje

--- src/com/curso/Run.java
+++ src/com/curso/Run.java
@@ -3,7 +3,9 @@ package com.curso;
 public class Run {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("In english: Hello World!");
+
+		System.out.println("En español: Hola Mundo!");
 
 	}
 
